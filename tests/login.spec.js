611043fca8299cd2d3b47ca45eeb1d39f37d3bb9
alexandre-bsantos <alexandre_bsantos12@hotmail.com>
2025-05-17 16:58:59 -0300

    Caso de teste de login com sucesso

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,32 @@
 import { test, expect } from '@playwright/test';
 
+import { obterCodigo2FA } from '../support/db';
+
 test('Não deve logar quando o codigo de autenticacao é invalido', async ({ page }) => {
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: "147258"
+  }
+
+  await page.goto('http://paybank-mf-auth:3000/');
+
+  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  for (const digito of usuario.senha) {
+    await page.getByRole('button', { name: digito }).click();
+  }
+
+  await page.getByRole('button', { name: 'Continuar' }).click();
+  
+  await page.getByRole('textbox', { name: '000000' }).fill('123456');
+  await page.getByRole('button', { name: 'Verificar' }).click();
+  
+  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
 
   const usuario = {
     cpf: '00000014141',
@@ -17,10 +43,18 @@ test('Não deve logar quando o codigo de autenticacao é invalido', async ({ pag
   }
 
   await page.getByRole('button', { name: 'Continuar' }).click();
+
+  // temporário
+  await page.waitForTimeout(3000)
+
+  const code = await obterCodigo2FA()
   
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
+  await page.getByRole('textbox', { name: '000000' }).fill(code);
   await page.getByRole('button', { name: 'Verificar' }).click();
   
-  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+  // temporário
+  await page.waitForTimeout(2000)
+
+  await expect(page.locator('#account-balance')).toHaveText('R$ 5.000,00')
 });
 
